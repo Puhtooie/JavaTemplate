e4d6308cda5a86344c6e30c2c1e1336aa3fff928
Puhtooie <44322930+Puhtooie@users.noreply.github.com>
2019-03-23 17:28:24 -0700

    Update MongoMain.java

--- MongoMain.java
+++ MongoMain.java
@@ -1,11 +1,10 @@
-//package com.mongodb;
+package com.mongodb;
 
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.QueryBuilder;
 import com.mongodb.DBCollection;
 
-import javax.swing.text.Style;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -19,31 +18,28 @@ public class MongoMain {
 
     public MongoMain(){}
 
-    public MongoMain(String collect){this.collect = collect;}
+    public MongoMain(String collect)
+    {
+        this.collect = collect; //This is so that more than one Collection can be instantiated and querried.
+    }
 
 
     public void getDb(String userName, String dataBase,
                         String password)
     {
         this.col = new Collection(userName,dataBase,
-                password).getCollection(collect);
+                password).getCollection(collect); //init the DBCollection
     }
 
 
-    public ArrayList Query(Date startDate, Date endDate, ArrayList params)
+    public ArrayList QueryBuild(Date startDate, Date endDate, ArrayList params)
     {
         DBObject query = new QueryBuilder().start(startDate)
-                .lessThan(endDate).and(new QueryBuilder()
-                        .or(Arrays.asList(params)))
+                .lessThan(endDate).and(new QueryBuilder() 
+                        .or(Arrays.asList(params)))//Params is an ArrayList of QueryBuilder() Objects
                 .get();
 
         return col.find(query);
-    }
-
-    public query(Date startDate, Date endDate, ArrayList params)
-    {
-
-        //for Aggregation.
     }
 
 }
