d7821793077dc310e42238c196a9ca3e6b83e83c
Puhtooie <44322930+Puhtooie@users.noreply.github.com>
2019-03-23 17:32:52 -0700

    Add files via upload

---
+++ Mongo/Collection.java
@@ -0,0 +1,32 @@
+//package com.mongodb;
+
+
+import java.util.Arrays;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
+public class Collection{
+    private String userName, dataBase, password;
+
+    public Collection(){}
+
+    public Collection(String userName, String dataBase,
+                      String password)
+    {
+        this.userName = userName;
+        this.dataBase = dataBase;
+        this.password = password;
+    }
+
+    public DB getDb()
+    {
+        MongoCredential credential = MongoCredential.createCredential(userName,
+                dataBase, password);
+
+        return new MongoClient(new ServerAddress("localhost", 27017),
+                Arrays.asList(credential))
+                .getDB(dataBase);
+    }
+}
